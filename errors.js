3c563afadb2505666e3bf19d04485a51671376a3
Nishant-Kumar-devz <nishantkumardevz@gmail.com>
2025-06-06 16:20:07 +0530

    Refactor BookingSDKError class to include statusCode option for improved error handling

--- errors.js
+++ errors.js
@@ -1,10 +1,9 @@
 class BookingSDKError extends Error {
   constructor(message, options = {}) {
     super(message);
-    this.name = "BookingSDKError"; // Set a specific name for easier identification
-    this.success = false; // Add your custom field
-    this.statusCode = options.statusCode || 500; // Optional: add a status code
-    // You can add more custom fields here if needed
+    this.name = "BookingSDKError";
+    this.success = false;
+    this.message = message;
   }
 }
 
